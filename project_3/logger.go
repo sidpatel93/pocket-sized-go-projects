6ef8a47b51156e8b3e249ef853e1645cb1851d4a
Siddharth Patel <sid.auburn@gmail.com>
2025-12-12 06:14:05 -0500

    feat: implement logger levels and methods for debug and info logging

--- project_3/logger.go
+++ project_3/logger.go
@@ -2,3 +2,24 @@ package logger
 
 type Logger struct {
 }
+
+// Level represents the available logger levels.
+type Level byte
+
+// Available logger levels enum.
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+)
+
+// Debugf formats and prints a message if the log level is debug or higher.
+func (l *Logger) Debugf(format string, args ...any) {
+	// implement me
+}
+
+// Infof formats and prints a message if the log level is info or higher.
+func (l *Logger) Infof(format string, args ...any) {
+	// implement me
+}
