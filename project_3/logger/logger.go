833829e61e07c04a79c90a430c5a71541b195f68
Siddharth Patel <sid.auburn@gmail.com>
2025-12-12 07:06:16 -0500

    fix: implement debug logging functionality in Logger

--- project_3/logger/logger.go
+++ project_3/logger/logger.go
@@ -1,5 +1,7 @@
 package logger
 
+import "fmt"
+
 type Logger struct {
 	threshold Level
 }
@@ -13,6 +15,10 @@ func New(threshold Level) *Logger {
 // Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
 	// implement me
+	if l.threshold > LevelDebug {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 // Infof formats and prints a message if the log level is info or higher.
