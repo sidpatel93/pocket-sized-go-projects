bdebfc78a33cd3dc371427c52705c03c75fe08db
Siddharth Patel <sid.auburn@gmail.com>
2025-12-12 07:29:56 -0500

    feat: enhance Logger to set default output to stdout if not provided

--- project_3/logger/logger.go
+++ project_3/logger/logger.go
@@ -1,20 +1,31 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+// Logger represents a simple logger with different log levels.
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
-func New(threshold Level) *Logger {
+// New creates a new Logger with the given threshold level and output writer.
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
 // Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
 	// implement me
+	if l.output == nil {
+		l.output = os.Stdout
+	}
 	if l.threshold > LevelDebug {
 		return
 	}
@@ -24,8 +35,22 @@ func (l *Logger) Debugf(format string, args ...any) {
 // Infof formats and prints a message if the log level is info or higher.
 func (l *Logger) Infof(format string, args ...any) {
 	// implement me
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold > LevelInfo {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 func (l *Logger) Errorf(format string, args ...any) {
-
+	// implement me
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold > LevelError {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
--- project_3/logger/logger_test.go
+++ project_3/logger/logger_test.go
@@ -1,12 +1,14 @@
 package logger_test
 
 import (
+	"os"
+
 	"github.com/sidpatel93/pocket-sized-go-projects/project_3/logger"
 	// "testing"
 )
 
 func ExampleLogger_Debugf() {
-	debugLogger := logger.New(logger.LevelDebug)
+	debugLogger := logger.New(logger.LevelDebug, os.Stdout)
 	debugLogger.Debugf("Hello, %s", "world")
 	// Output: Hello, world
 }
