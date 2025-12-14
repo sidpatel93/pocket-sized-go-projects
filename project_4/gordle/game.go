f584cf936f3b06e5dcb0d3a792e15cd4c54e5928
Siddharth Patel <sid.auburn@gmail.com>
2025-12-14 16:59:41 -0500

    working on the gordle game

---
+++ project_4/gordle/game.go
@@ -0,0 +1,15 @@
+package gordle
+
+// Game represents a single instance of a Gordle game.
+type Game struct {
+}
+
+// New creates and returns a new instance of a Gordle game.
+func New() *Game {
+	g := &Game{}
+	return g
+}
+
+func (g *Game) Play() {
+
+}
---
+++ project_4/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/sidpatel93/pocket-sized-go-projects/project_4/gordle"
+
+func main() {
+	gordleGame := gordle.New()
+	gordleGame.Play()
+}
