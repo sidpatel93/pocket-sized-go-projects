5ec3a34c06c4b982cb701f94fa435221e0a7d575
Siddharth Patel <sid.auburn@gmail.com>
2025-12-15 07:08:27 -0500

    feat: enhance input handling in Gordle game by validating guess length

--- project_4/gordle/game.go
+++ project_4/gordle/game.go
@@ -2,13 +2,17 @@ package gordle
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"os"
 )
 
 // Game represents a single instance of a Gordle game.
 type Game struct {
 	reader bufio.Reader
 }
+
+const solutionLength = 5
 
 // New creates and returns a new instance of a Gordle game.
 func New(playerInput io.Reader) *Game {
@@ -19,8 +23,21 @@ func New(playerInput io.Reader) *Game {
 }
 
 // ask reader input until a valid suggestion is made
-func (g *Game) ask() {
-
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
+	for {
+		input, _, err := g.reader.ReadLine()
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
+			continue
+		}
+		guess := []rune(string(input))
+		if len(guess) != solutionLength {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution! Expected %d characters, got %d.\n", solutionLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
 
 func (g *Game) Play() {
