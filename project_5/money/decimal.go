8028ad4f364689af2e0fd6aaf643a8ffae991945
Siddharth Patel <sid.auburn@gmail.com>
2025-12-19 07:31:39 -0500

    enhance: add detailed comments and structure to Decimal type for clarity

--- project_5/money/decimal.go
+++ project_5/money/decimal.go
@@ -1,3 +1,12 @@
 package money
 
-type Decimal struct{}
+// Decimal can represent a floating-point number with a fixed precision.
+// example:
+// 1.52 = 152 * 10^(-2) will be stored as {152, 2}
+type Decimal struct {
+	// subunits is the amount of subunits.
+	// Multiply it by the precision to get the real value
+	subunits int64
+	// Number of "subunits" in a unit, expressed as a power of 10.
+	precision byte
+}
