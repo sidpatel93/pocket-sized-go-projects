b1c4db2361bff55035df5c76521b87a8c42bbd75
Siddharth Patel <sid.auburn@gmail.com>
2025-12-19 06:12:56 -0500

    created the empty types for the project

---
+++ project_5/go.mod
@@ -0,0 +1,3 @@
+module github.com/sidpatel93/pocket-sized-go-projects/project_5
+
+go 1.25.5
---
+++ project_5/money/convert.go
@@ -0,0 +1,5 @@
+package money
+
+func Convert(amount Amount, to Currency) (Amount, error) {
+	return Amount{}, nil
+}
