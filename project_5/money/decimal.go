8ca84c68b932b19ef68d55a7407013fd3205b81c
Siddharth Patel <sid.auburn@gmail.com>
2025-12-20 17:24:48 -0500

    fix: correct function name and improve ParseDecimal implementation

--- project_5/money/decimal.go
+++ project_5/money/decimal.go
@@ -1,5 +1,10 @@
 package money
 
+import (
+	"strconv"
+	"strings"
+)
+
 // Decimal can represent a floating-point number with a fixed precision.
 // example:
 // 1.52 = 152 * 10^(-2) will be stored as {152, 2}
@@ -9,4 +14,23 @@ type Decimal struct {
 	subunits int64
 	// Number of "subunits" in a unit, expressed as a power of 10.
 	precision byte
+}
+
+func ParseDecimmal(s string) (Decimal, error) {
+	// split string on the decimal point
+	before, after, found := strings.Cut(s, ".")
+	var subunits int64
+	if !found {
+		// parse before as integer
+		subunits, _ = strconv.ParseInt(before, 10, 64)
+	}
+	subunits, err := strconv.ParseInt(before+after, 10, 64)
+	if err != nil {
+		return Decimal{}, err
+	}
+	precision := byte(len(after))
+	return Decimal{
+		subunits:  subunits,
+		precision: precision,
+	}, nil
 }
